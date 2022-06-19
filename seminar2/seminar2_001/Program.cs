a1a60bc629f590f425d8be12131303f4de8d4edc
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-19 21:49:35 +0300

    added seminar2 folder

---
+++ seminar2/seminar2_001/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 1. Напишите программу, которая принимает
+на вход трёхзначное число и на выходе
+показывает последнюю цифру этого числа.*/
+
+// Код написан для числа с любым количеством знаков .
+
+Console.Write("Enter the number > ");
+
+string number = Console.ReadLine();
+
+string[] arrow = { number };
+
+Console.Write("The last digit of this number: " + arrow[0][number.Length - 1]);
+
+
