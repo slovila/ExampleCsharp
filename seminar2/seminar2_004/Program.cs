b76bf0c73e7c0239321a130bf19157f3fc09dca8
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-20 07:30:24 +0300

    seminar2_004

---
+++ seminar2/seminar2_004/Program.cs
@@ -0,0 +1,12 @@
+﻿/*4. Напишите программу, которая принимает на вход
+число и проверяет, кратно ли оно одновременно 7 и
+23.*/
+
+Console.Write("Введите число > ");
+int namber = Convert.ToInt32(Console.ReadLine());
+if((namber % 7 == 0) ||(namber % 23 == 0) ) Console.WriteLine(namber + " кратно и 7 и 23 " );
+
+else 
+{
+    Console.WriteLine(namber + " не кратно и 7 и 23 одновременно " );
+}
