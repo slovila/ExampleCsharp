fd937c442b15980d90e43896ce371f1e820e3f99
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-26 08:04:33 +0300

    DOP_31

--- seminar2/DOP_21/Program.cs
+++ seminar2/DOP_21/Program.cs
@@ -16,3 +16,25 @@ while (count < a)
 }
 
 Console.WriteLine("Максимальная цифра случайного числа > " + max);
+
+
+/*int Rnd(int min, int max)
+{
+    Random rnd = new Random();
+    return rnd.Next(min, max+1);
+} 
+
+int a = Rnd(10,9999);
+Console.WriteLine (a);
+int max = 0;
+
+ while ( a > 0)
+ {
+  int t1 = a%10;
+  if (t1 > max) max = t1;
+ a = a/10;
+ 
+ }
+
+ Console.WriteLine (max);
+*/
---
+++ seminar2/DOP_31/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Введите пять чисел > ");
+while(a=5)
+{
+    
+}
+
+
+
+//string number = Convert.ToString(new Random().Next(10000, 99999));
+string[] arrow = { number };
+Console.WriteLine(arrow[0]);
+var a = arrow[0].Length;
+int count = 0;
+var sum = 0.0;
+double am = 0.0;
+while (count < a)
+{
+    sum = (sum - '0') + arrow[0][count];
+    count++;
+}
+am = sum / a;
+Console.WriteLine(sum);
+Console.WriteLine(am);
