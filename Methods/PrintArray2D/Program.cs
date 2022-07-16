9328f81c1159b97c22aed04b5f0722f8df40ef1a
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-07-16 13:55:07 +0300

    PrintArray2D

---
+++ Methods/PrintArray2D/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
