ff8fe37f1b1966533ec5aa5f8aadb8bda37da234
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-07-16 15:11:52 +0300

    Task2

---
+++ seminar8/Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 1: Задайте двумерный массив. 
+//Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GenerateArray(int rows, int columns, int min, int max)
+{
+    int[,] answer = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            answer[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return answer;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+void ChangeArrayLine(int[,] array)
+{
+  for( int i = 0 ; i < array.GetLength(1); i++)
+  {
+     int temp = array[0 , i];
+
+    array[0,i]=array[array.GetLength(0)-1, i];
+    
+    array[array.GetLength(0)-1, i] = temp;
+  }
+ 
+
+}
+
+int[,]array1 = GenDobMass(3,4);
+PrintArray(array1);
+ChangeArrayLine(array1);
+Console.WriteLine();
+PrintArray(array1);
---
+++ seminar8/Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿/ Задайте двумерный массив. Напишите программу, 
+//которая заменяет строки на столбцы. В случае, 
+//если это невозможно, программа должна вывести сообщение 
+//для пользователя.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int [,] GenDobMass (int m, int n )
+{
+    int[,] Array  = new int[m,n];
+    for( int i =0; i < Array.GetLength(0); i++)
+    {
+        for( int j =0; j < Array.GetLength(1); j++)
+         {
+            Array[i,j]= new Random().Next(0,10);
+
+         }
+    }
+            return Array;
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+    }
+
+    System.Console.WriteLine();
+}
+
+void CangeArrayLine2(int[,] array)
+{
+  for( int i = 0 ; i < array.GetLength(0); i++)
+  {
+    for( int j = i ; j < array.GetLength(1) ; j++)
+    {
+        int temp = array[i , j];
+        array[i,j]=array[j , i];
+    
+        array[j,i] = temp;
+    }
+     
+  }
+ 
+}
+
+int[,]array1 = GenDobMass(3,3);
+PrintArray(array1);
+CangeArrayLine2(array1);
+Console.WriteLine();
+PrintArray(array1);
