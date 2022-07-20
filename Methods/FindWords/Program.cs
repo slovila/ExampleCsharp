bd596f524e49308cf2b6e32b1fd0b6786c50f728
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-07-20 17:36:44 +0300

    FindWords

---
+++ Methods/FindWords/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0 )
+{
+    if (length==word.Length)
+    {
+        System.Console.WriteLine($"{n++} {new string (word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length+1);
+    }
+}
+
+FindWords("asdf", new char[5]);
